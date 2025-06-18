8c59917b19f69c3cf4841cb5f49f712dc459f349
Bennnii <74566368+Bennnii@users.noreply.github.com>
2025-06-19 00:26:32 +0200

    Update index.py
    Test

--- index.py
+++ index.py
@@ -1,17 +1,13 @@
 from nextcord.ext import commands
-from discord_ui import UI
 from commands import normal_commands
 
-TOKEN = 'TEST_TOKEN'
+TOKEN = 'Nzg4ODIzMTc4OTQ3NDYxMTIw.GWhQ9-.5wS0qvfOf2iUjdmCMYcpXbrP0EQFSPOBJcy2J0'
 
 client = commands.Bot(command_prefix = "haru!")
-client.ui = UI(client, {'auto_sync': False, "sync_on_cog": True})
-
-normal_commands.register(client, client.ui)
 
 @client.event
 async def on_ready():
-    print("Ready! Pterodactyl")
+    print("Successfully started instance✔️")
     print("\n-----------------------------------------------")
     print(f"{client.user} is now online!")
     print("All systems are running!")
